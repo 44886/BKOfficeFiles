9609df490e653a70f505e59f6abeac62394c0de4
boy <boy@44886.com>
2025-02-12 20:26:43 +0800

    2025.02.13

--- 2025.02.10/_修复com注册错误.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-
-reg delete "HKEY_CLASSES_ROOT\TypeLib\{00020905-0000-0000-C000-000000000046}\8.7" /f
